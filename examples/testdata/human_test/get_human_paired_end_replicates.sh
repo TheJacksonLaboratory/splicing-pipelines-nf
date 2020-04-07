34c13a8305a139fc5119b1c69974ffe95f8d0e72
adeslatt <adeslat@jax.org>
2020-04-07 06:57:13 -0400

    adjusted directory to /projects/anczukow-lab and added human_test data

--- examples/testdata/human_test/get_human_paired_end_replicates.sh
+++ examples/testdata/human_test/get_human_paired_end_replicates.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# Pre-requisite - these data need to be downloaded by an authorized and authenticated user 
+# on the google cloud platform
+#
+# gcloud auth login
+# then this command may be run.
+#
+
 gsutil -m cp gs://cloudosinputdata/inputs/splicing-pipelines-nf/samples/SRR1039513_1.fastq.gz .
 gsutil -m cp gs://cloudosinputdata/inputs/splicing-pipelines-nf/samples/SRR1039513_2.fastq.gz .
 gsutil -m cp gs://cloudosinputdata/inputs/splicing-pipelines-nf/samples/SRR1039509_1.fastq.gz .
