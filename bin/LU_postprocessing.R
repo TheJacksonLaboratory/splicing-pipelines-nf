2469fe7fe617b920e9d1bcdadef501dd1d479212
lmurba <41591813+lmurba@users.noreply.github.com>
2020-05-22 15:42:47 -0400

    Update sign of dPSI

--- bin/LU_postprocessing.R
+++ bin/LU_postprocessing.R
@@ -247,6 +247,7 @@ get_postprocessing_table <- function(df){
   df$mSC2 <- sapply(df$SJC_SAMPLE_2, function(x) calc_mean(x))
   df$mPSI1 <- sapply(df$IncLevel1, function(x) calc_mean(x))
   df$mPSI2 <- sapply(df$IncLevel2, function(x) calc_mean(x))
+  df$IncLevelDifference <- df$IncLevelDifference*(-1)
   # reorder and rename columns, keeping only the ones we want
   final_df <- df[,c("Event_type", "short_ID", "long_ID", "geneSymbol", "GeneID", "strand", "IJC_SAMPLE_1", "SJC_SAMPLE_1", 
                     "IJC_SAMPLE_2", "SJC_SAMPLE_2", "IncLevel1", "IncLevel2", "mIC1", "mSC1", "mIC2", "mSC2", "mPSI1", "mPSI2",
