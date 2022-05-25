38458dda14d624c17d20806a457e788a7a426c25
Brittany Angarola <62404570+angarb@users.noreply.github.com>
2022-05-25 17:40:07 -0400

    Update LU_postprocessing.R
    Addressing Issue #309

--- original_scripts/postprocessing/LU_postprocessing.R
+++ original_scripts/postprocessing/LU_postprocessing.R
@@ -339,7 +339,7 @@ b2_file <- config[config$parameter=="b2", "value"]
 b1_list <- t(read.table(b1_file, sep=","))
 b2_list <- t(read.table(b2_file, sep=","))
 
-bam_list_df <- merge(b1_list, b2_list, by="row.names", suffixes = c("_b1", "_b2"))
+bam_list_df <- merge(b1_list, b2_list, by="row.names", all= T, sort = F, suffixes = c("_b1", "_b2"))
 bam_list_df <- bam_list_df[,-1]
 colnames(bam_list_df) <- lapply(colnames(bam_list_df), function(x){
   new_val <- strsplit(x, "_")[[1]][2]
